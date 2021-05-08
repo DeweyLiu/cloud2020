f2ed7659bbeae25aca8a836f511d79686989b6ca
Dewey <378648742@qq.com>
2021-05-08 18:13:26 +0800

    子工程创建

---
+++ cloud-provider-payment8001/src/main/java/com/codewey/springcloud/payment8001.java
@@ -0,0 +1,12 @@
+package com.codewey.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dewey
+ * @version 1.0.0
+ * @date 2021/5/8 14:13
+ */
+@SpringBootApplication
+public class payment8001 {
+}
